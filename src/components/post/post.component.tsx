3afcf84a26afd98c21d40c168f426bd2a67b0dcf
Mradul Singhal <singhalmradul@gmail.com>
2024-02-20 16:23:08 -0500

    Update font and style for high-five button and share button

--- src/components/post/post.component.tsx
+++ src/components/post/post.component.tsx
@@ -1,19 +1,26 @@
 import './post.styles.css';
 import { Post as PostType } from '../../store/posts/posts.types';
-
+import { ReactComponent as HighFive } from "../../assets/high-five.svg";
+import { ReactComponent as Comment } from "../../assets/comment.svg";
 
 const Post = ({ user: { username, profilePicture }, text, image, highFives, comments}: PostType) => {
 	return (
 		<div className='post'>
 			<div className='post-header'>
 				<img src={profilePicture} alt={username} />
-			<h2>{username}</h2>
+			<p>{username}</p>
 			</div>
 			<p>{text}</p>
 			{image && <img src={image} alt={text} />}
 			<div className='buttons'>
-				<button>{highFives}</button>
-				<button>{comments}</button>
+				<div>
+					<HighFive/>
+					<span>{highFives}</span>
+				</div>
+				<div>
+					<Comment />
+					<span>{comments}</span>
+				</div>
 			</div>
 		</div>
 	);
